fb37e9e524f4612958750a031d9440d7b083ca85
Matt Schultz <matt@mattschultz.info>
2022-05-02 19:20:49 +0930

    Add grommet and webext-options-sync packages

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -1,6 +1,23 @@
 import {browser} from 'webextension-polyfill-ts';
+import OptionsSync from 'webext-options-sync';
+
+const options = new OptionsSync({
+  defaults: {
+    colorString: 'green',
+    anyBooleans: true,
+    numbersAreFine: 9001,
+  },
+
+  // List of functions that are called when the extension is updated
+  // migrations: [
+  // Integrated utility that drops any properties that don't appear in the defaults
+  // OptionsSync.migrations.removeUnused,
+  // ],
+});
 
 browser.runtime.onInstalled.addListener((): void => {
   console.log(browser.i18n.getMessage('installationLog'));
   browser.runtime.openOptionsPage();
 });
+
+export {options};
